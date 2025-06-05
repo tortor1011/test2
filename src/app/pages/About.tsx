c31661948dc080d0c133e56e4b52f26b03c52bd5
tortor1011 <raung1411@gmail.com>
2025-06-05 16:26:43 +0700

    set align component center

--- src/app/pages/About.tsx
+++ src/app/pages/About.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Box, Button, Container, Typography, AppBar, Toolbar, Link } from '@mui/material';
+import { Box, Button, Container, Typography, AppBar, Toolbar, Link,Grid } from '@mui/material';
 import Image from 'next/image';
 import Head from 'next/head';
 import house from '../../../public/image/house.jpg'
@@ -108,39 +108,38 @@ export default function About() {
         </Button>
       </Box>
 
-       <Box 
+       {/* <Container maxWidth={false} 
   sx={{
     display: { xs: 'none', md: 'flex', lg: 'flex' },
     justifyContent: "center",
-    mb: 12,
-    px: { md: 2, lg: 4, xl: 6 }, 
+    alignItems:"center",
+    maxWidth:"100%"
   }}
 > 
  
-  <Box>
+  
     <Typography
       variant="h2"
       fontWeight={700}
       sx={{ 
-        mt: 15,
-        mr: { md: 0, lg: 5, xl: 16 },
-        p: 5,
         display: { xs: 'none', md: 'flex', lg: 'flex' },
         fontFamily: "Playfair Display",
         color: "#0E204E",
         textDecoration: "none",
         textWrap: "wrap", 
-        lineHeight: '1.2', 
+        lineHeight: '1.2',
+        maxWidth:"300px", 
+        width:"20%"
       }}
     >
       About <br /> Pineapple <br /> Island
     </Typography>
-  </Box>
+  
 
   
   <Box sx={{ 
-    width: { md: 600, lg: 720, xl: 800 }, 
-    height: { md: 500, lg: 620, xl: 700 },
+    maxWidth:"800px", 
+    maxHeight:"700px",
     position: 'relative',
   }}>
     <Swiper
@@ -203,13 +202,122 @@ export default function About() {
       happiness.
     </Typography>
   </Box>
-</Box>
-      
-      
-    
-        
-    
+</Container> */}
+      <Container maxWidth={false}>
+        <Container maxWidth={false}
+          sx={{
+            display: { xs: "none", md: "flex", lg: "flex" },
+            maxWidth: "100%",
+            pl: 5,
+            alignItems:"center",
+            justifyContent:"center"
 
+          }}
+        >
+          <Box sx={{
+            display: { xs: "none", md: "flex", lg: "flex" },
+            width: '30%',
+            height: '100%',
+            alignItems: "center",
+            justifyContent:"center"
+          }}>
+  <Typography
+      variant="h2"
+      fontWeight={700}
+      sx={{ 
+        display: { xs: 'none', md: 'flex', lg: 'flex' },
+        fontFamily: "Playfair Display",
+        color: "#0E204E",
+        textDecoration: "none",
+        textWrap: "wrap", 
+        lineHeight: '1.2',
+        maxWidth:"300px", 
+
+      }}
+    >
+      About <br /> Pineapple <br /> Island
+    </Typography>
+             
+
+          </Box>
+
+
+          <Grid
+
+            sx={{
+              display: { xs: "none", md: "flex", lg: "flex" },
+              justifyContent: "center",
+              alignContent: "center",
+              width: '50%',
+              alignItems: "center"
+            }}
+          >
+            <Box sx={{ 
+    maxWidth:"100%", 
+    maxHeight:"700px",
+    position: 'relative',
+  }}>
+    <Swiper
+      modules={[Navigation]}
+      navigation
+      spaceBetween={20}
+      slidesPerView={1}
+      loop={true}
+    >
+      {[road, rightslide, leftslide].map((imgSrc, index) => (
+        <SwiperSlide key={index}>
+          <Image
+            alt={`slide-${index}`}
+            src={imgSrc}
+            priority
+            width={720}
+            height={552}
+            style={{ 
+              objectFit: 'cover',
+              maxWidth:"100%",
+              width: '100%',
+              minHeight: '620px',
+            }}
+          
+       
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  </Box>
+          </Grid>
+          <Box sx={{
+    width: "20%", 
+    height: { md: '620px', lg: '620px', xl: '620px' }, 
+    bgcolor: "#0E204E !important",
+    display: { xs: 'none', md: 'flex', lg: 'flex' },
+    alignItems:"center",
+  }}>
+    <Typography 
+      variant="h6" 
+      noWrap
+      sx={{
+        ml: 0,
+        p: 5,
+        color: 'white',
+        fontFamily: "Manrope",
+        display: { xs: "none", md: "flex", lg: "flex" },
+        textWrap: "wrap",
+        textDecoration: "none",
+        fontSize: { md: 16, lg: 18, xl: 20 }, 
+        lineHeight: '1.5', 
+      }}
+    >
+      Pineapple Island is the most sought-after community in Ibadan that
+      values and puts the needs of their clients first. It is a territory
+      with loft complexes that creates a country life and homely feeling
+      that is integrated with industrial development. A perfect,
+      professional built abode that gives you the true taste of beauty and
+      happiness.
+    </Typography>
+  </Box>
+        </Container>
+      </Container>
     </>
   );
 }
